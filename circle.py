f8d88067982ded290ac9c04623be38f662070374
pushpendra2607 <puspendrasingh26072005@gmail.com>
2026-04-21 10:05:21 +0530

    Implement judgeCircle method to validate moves

---
+++ circle.py
@@ -0,0 +1,6 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        c=Counter(moves)
+        if(c['L']==c['R'])  and (c['U']==c['D']):
+            return True
+        return False
