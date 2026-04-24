1611a282bcd0a98082aa2548c42acc3a15fd5d71
pushpendra2607 <puspendrasingh26072005@gmail.com>
2026-04-25 00:36:49 +0530

    Create finfmiddiff.py

---
+++ finfmiddiff.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMinDiff(self, arr, m):
+        n = len(arr)
+        
+        if m == 0 or n == 0 or m > n:
+            return 0
+        
+        arr.sort()
+        ans = float('inf')
+        
+        for i in range(n - m + 1):
+            ans = min(ans, arr[i + m - 1] - arr[i])
+        
+        return ans
